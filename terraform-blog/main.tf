f5c7b4060e54e04bd943a8fc693f632eedd7032a
safwenketata <81825117+safwenketata@users.noreply.github.com>
2023-05-25 12:41:11 +0100

    Update main.tf

--- terraform-blog/main.tf
+++ terraform-blog/main.tf
@@ -24,7 +24,7 @@ provider "proxmox" {
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "Kube-master" {
-  count = 0 # just want 1 for now, set to 0 and apply to destroy VM
+  count = 1 # just want 1 for now, set to 0 and apply to destroy VM
   name = "Master-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
@@ -81,7 +81,7 @@ resource "proxmox_vm_qemu" "Kube-master" {
 #
 # we are looking to create a proxmox_vm_qemu entity named kube-agent
 resource "proxmox_vm_qemu" "kube-agent" {
-  count = 0 # just want 1 for now, set to 0 and apply to destroy VM
+  count = 2 # just want 1 for now, set to 0 and apply to destroy VM
   name = "kube-agent-0${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
